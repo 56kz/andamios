00f3f964671eba470fb117813ba94385dfc87dbb
khris <khris56@hotmail.com>
2019-05-02 19:48:48 -0500

    Customer new and create

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,22 @@ class CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to customers_path, notice: "El cliente fue creado con éxito"
+    else
+      render :new
+    end
+  end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:name, :company, :email, :phone)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '/products', to: 'products#index'
   get '/sectors', to: 'sectors#index'
   get '/customers', to: 'customers#index'
+  get '/customers/new', to: 'customers#new', as:'new_customer'
+  post '/customers', to: 'customers#create'
   get '/sales', to: 'sales#index'
 end
