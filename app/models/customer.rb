f41e27459cb1d052e6228b44338073f9bda226d6
khris <khris56@hotmail.com>
2019-05-02 21:34:41 -0500

    ok

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: customers
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  company    :string
+#  email      :string
+#  phone      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Customer < ActiveRecord::Base
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  quantity   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Product < ActiveRecord::Base
 end
