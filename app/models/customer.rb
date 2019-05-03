435e9a617bb886fd177486b870ad1726a8d5f586
khris <khris56@hotmail.com>
2019-05-02 21:54:31 -0500

    relation costumers sectors

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,7 +9,9 @@
 #  phone      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  sector_id  :integer
 #
 
 class Customer < ActiveRecord::Base
+  belongs_to :sector
 end
